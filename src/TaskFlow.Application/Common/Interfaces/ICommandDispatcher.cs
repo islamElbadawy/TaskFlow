e9caf64bf2a3071973363c1f4da4d222c933be7b
islamElbadawy <islam.201211@gmail.com>
2026-01-24 20:19:52 +0300

    Prepare CQRS Interfaces

---
+++ src/TaskFlow.Application/Common/Interfaces/ICommandDispatcher.cs
@@ -0,0 +1,6 @@
+﻿namespace TaskFlow.Application.Common.Interfaces;
+
+public interface ICommandDispatcher
+{
+    Task<TResult> DispatchAsync<TResult>(ICommand<TResult> command, CancellationToken cancellationToken = default);
+}
---
+++ src/TaskFlow.Application/Common/Interfaces/IQueryDispatcher.cs
@@ -0,0 +1,6 @@
+﻿namespace TaskFlow.Application.Common.Interfaces;
+
+public interface IQueryDispatcher
+{
+    Task<TResult> DispatchAsync<TResult>(IQuery<TResult> query, CancellationToken cancellationToken = default);
+}
