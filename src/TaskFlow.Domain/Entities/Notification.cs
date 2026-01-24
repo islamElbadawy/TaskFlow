51bca34280ef00c0c645ad52fab2abb0b0f8ec8c
islamElbadawy <islam.201211@gmail.com>
2026-01-24 17:23:31 +0300

    Initiate solution and add entities

---
+++ src/TaskFlow.Domain/Entities/Notification.cs
@@ -0,0 +1,16 @@
+﻿using TaskFlow.Domain.Common;
+using TaskFlow.Domain.Enums;
+
+namespace TaskFlow.Domain.Entities;
+
+public class Notification : BaseEntity
+{
+    public string Message { get; set; } = string.Empty;
+    public NotificationType Type { get; set; }
+    public bool IsRead { get; set; } =  false;
+    
+    
+    public int UserId { get; set; }
+
+    public User User { get; set; } = null!;
+}
---
+++ src/TaskFlow.Domain/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using TaskFlow.Domain.Common;
+using TaskFlow.Domain.Enums;
+
+namespace TaskFlow.Domain.Entities;
+
+public class User : BaseEntity
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public UserRole Role { get; set; } = UserRole.User;
+    public bool IsActive { get; set; } = true;
+
+
+    public ICollection<TaskItem> CreatedTasks { get; set; } = new List<TaskItem>();
+    public ICollection<TaskItem> AssignedTasks { get; set; } = new List<TaskItem>();
+    public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+    public ICollection<Notification> Notifications { get; set; } = new List<Notification>();
+
+
+    public string FullName => $"{FirstName} {LastName}";
+}
