f5353cb71c6c22b89860d536cb8ba47782575912
islamElbadawy <islam.201211@gmail.com>
2026-01-28 01:30:27 +0300

    Add Update and Delete User Endpoints

--- src/TaskFlow.API/obj/Debug/net10.0/TaskFlow.API.AssemblyInfo.cs
+++ src/TaskFlow.API/obj/Debug/net10.0/TaskFlow.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskFlow.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4317b30f215efda58e40f86b358d5f92ee47a20e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ee09d2fae75c892643faeca13ccf76c2f5bd7b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskFlow.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskFlow.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
